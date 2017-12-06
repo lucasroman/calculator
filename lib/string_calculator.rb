b8c3c8b223d300e2aed4e51e847415fa29f2a774
Lucas Roman <lucasroman@gmail.com>
2017-12-06 13:56:15 -0300

    Refactor method 'add'

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,10 +1,6 @@
 class StringCalculator
 
   def self.add(input)
-    if input.empty?
-      0
-    else
-      input.split(',').map(&:to_i).inject(&:+)
-    end
+    input.split(',').map(&:to_i).inject(0, &:+)
   end
 end
